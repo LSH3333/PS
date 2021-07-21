b945b10670310c72233da2c558358d3f567bd762
Jake <dltpgustpgus@gmail.com>
2021-07-21 14:29:35 +0900

    2021.07.21

--- 1058.cpp
+++ 1058.cpp
@@ -1,30 +1,20 @@
 #include <iostream>
+#include <algorithm>
 #include <vector>
 #include <queue>
-#include <algorithm>
 using namespace std;
 
 int n;
-char input[51][51];
-bool mark[51];
-vector<int> v[51];
-
 int ans = 0;
-
-// bool mark[] 배열 false로 초기화
-void InitMark()
-{
-    for(int i = 0; i < n; i++)
-    {
-        mark[i] = false;
-    }
-}
+// 인접리스트 형태로
+vector<int> edge[51];
+bool mark[51];
 
 int bfs(int idx)
 {
-    InitMark();
-
-    // (idx, depth)
+    // 다른 bfs 탐색을 위해 초기화
+    fill_n(mark, n, false);
+    int cnt = 0;
     queue<pair<int,int>> q;
 
     q.push({idx,0});
@@ -32,68 +22,49 @@ int bfs(int idx)
 
     while(!q.empty())
     {
-        int ver = q.front().first;
+        int v = q.front().first;
         int depth = q.front().second;
         q.pop();
 
-        // 탐색하는 기준 정점으로 부터 2이상 떨어져있으면 2친구가 아니므로 break
+        // 탐색하는 기준 정점으로 부터 2이상 떨어져있으면 2친구가 아니다
         if(depth == 2) break;
 
-        for(auto x : v[ver])
+        for(int x : edge[v])
         {
             // 이미 방문했다면 continue
             if(mark[x]) continue;
 
             mark[x] = true;
-            // 방문하는 정점과 깊이를 큐에 푸쉬
+            // 방문하는 정점,깊이를 큐에 푸쉬
             q.push({x, depth+1});
+            // 친구수 카운트 증가
+            cnt++;
         }
     }
-
-    // 방문한 정점들의 수
-    int res = 0;
-    for(int i = 0; i < n; i++)
-    {
-        res += mark[i];
-    }
-
-    // 자기 자신은 빼고 카운트
-    return res-1;
+    return cnt;
 }
 
 int main()
 {
+    ios::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    // inputs
     cin >> n;
-
-    for(int i = 0; i < n; i++)
-    {
-        scanf("%s", input[i]);
-    }
-    /*
+    //
     for(int i = 0; i < n; i++)
     {
         for(int j = 0; j < n; j++)
         {
-            cout << input[i][j];
-        } cout << endl;
-    }
-     */
-
-    for(int i = 0; i < n; i++)
-    {
-        for(int j = 0; j < n; j++)
-        {
-            if(input[i][j] == 'Y')
-                v[i].push_back(j);
+            char c;
+            cin >> c;
+            if(c == 'Y')
+            {
+                edge[i].push_back(j);
+            }
         }
     }
-    /*
-    for(int i = 0; i < n; i++)
-    {
-        for(auto x : v[i]) cout << x << ' ';
-        cout << endl;
-    }
-     */
 
     for(int i = 0; i < n; i++)
     {
