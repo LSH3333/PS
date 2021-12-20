eea1ee39262a5b9b8227fa71ab71f707405a5ff6
LSH3333 <dltpgustpgus@gmail.com>
2021-12-20 17:51:18 +0900

    2021.12.20

---
+++ works/1476.cs
@@ -0,0 +1,35 @@
+using static System.Console;
+using System;
+using System.Collections;
+
+namespace Project1
+{
+    public class Class1
+    {
+        public static void Main(string[] args)
+        {
+            int E, S, M;
+            string[] s_tmp = ReadLine().Split();
+            E = int.Parse(s_tmp[0]); S = int.Parse(s_tmp[1]); M = int.Parse(s_tmp[2]);
+
+            int cnt = 1;
+            int e = 1, s = 1, m = 1;
+            while(true)
+            {
+                if (e == E && s == S && m == M) break;
+                cnt++;
+
+                if (e < 15) e++;
+                else e = 1;
+
+                if (s < 28) s++;
+                else s = 1;
+
+                if (m < 19) m++;
+                else m = 1;
+            }
+            Write(cnt);
+
+        }
+    }
+}
