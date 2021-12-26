c6c0b43664b419cd4ee8eb96839f48726bcbfd58
LSH3333 <dltpgustpgus@gmail.com>
2021-12-26 16:00:19 +0900

    2021.12.26

--- programmers/디스크컨트롤러.cpp
+++ programmers/디스크컨트롤러.cpp
@@ -18,55 +18,34 @@ int solution(vector<vector<int>> jobs) {
     int answer = 0;
 
     sort(jobs.begin(), jobs.end());
+    priority_queue<vector<int>,vector<vector<int>>, cmp> pq;
 
-//
-//    for(auto x : jobs)
+//    while(!pq.empty())
 //    {
-//        cout << x[0] << ' ' << x[1] << endl;
+//        cout << pq.top()[0] << ' ' << pq.top()[1] << endl;
+//        pq.pop();
 //    }
 
     int time = 0;
     int idx = 0;
-    while(idx < jobs.size())
+    while(idx < jobs.size() || !pq.empty())
     {
-        priority_queue<vector<int>,vector<vector<int>>, cmp> pq;
-        for(int i = idx; i < jobs.size(); i++)
+        if(idx < jobs.size() && jobs[idx][0] <= time)
         {
-            if(jobs[i][0] > time) break;
-            if(jobs[i][0] <= time) { pq.push(jobs[i]); }
+            pq.push(jobs[idx++]);
+            continue;
         }
-        idx++;
 
         if(pq.empty()) time = jobs[idx][0];
         else
         {
-            cout << "idx: " << idx << endl;
-            cout << pq.top()[0] << ' ' << pq.top()[1] << endl;
             time += pq.top()[1];
             answer += time - pq.top()[0];
             pq.pop();
         }
     }
 
-    cout << answer/jobs.size() << endl;
+//    cout << answer/jobs.size() << endl;
     answer = answer / jobs.size();
     return answer;
-}
-
-int main()
-{
-    vector<vector<int>> jobs =
-            {
-                    {0,3}, {1,9}, {2,6}
-            };
-
-    vector<vector<int>> jobs2 =
-            {
-                    {0,3}, {0,4}, {1,9}, {2,6}, {7,1}
-            };
-    vector<vector<int>> jobs3 =
-            {
-                    {0,10}, {4,10}, {15,2}, {5,11}
-            };
-    solution(jobs3);
 }
