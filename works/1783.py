073e7f22f357f29a9fc74f8252e8c8ff330bce5b
LSH3333 <dltpgustpgus@gmail.com>
2021-12-28 15:47:21 +0900

    2021.12.28

---
+++ works/1783.py
@@ -0,0 +1,13 @@
+N, M = map(int, input().split())
+
+if N <= 1:
+    print(1)
+elif N <= 2:
+    if M >= 9: print(4)
+    else: print((M-1) / 2 + 1)
+elif M < 5:
+    print(M)
+elif M <= 5:
+    print(M-1)
+elif M > 5:
+    print(M-2)
