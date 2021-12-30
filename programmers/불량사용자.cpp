1333ebf5f70b87b1865efd1f8912abf9946be2fe
LSH <dltpgustpgus@gmail.com>
2021-12-30 20:21:31 +0900

    2021.12.30

--- programmers/불량사용자.cpp
+++ programmers/불량사용자.cpp
@@ -27,12 +27,12 @@ bool IsItBannedID(string banned, string id)
 
 void dfs(int idx)
 {
-    string target = bannedId[idx];
     if(idx == bannedId.size())
     {
         ans.push_back(v);
         return;
     }
+    string target = bannedId[idx];
 
     for(int i = 0; i < userId.size(); i++)
     {
@@ -55,19 +55,13 @@ int solution(vector<string> user_id, vector<string> banned_id) {
 
     for(auto x : user_id) userId.push_back(x);
     for(auto x : banned_id) bannedId.push_back(x);
+
     dfs(0);
 
     for(auto &x : ans)
     {
         sort(x.begin(), x.end());
     }
-//    for(auto x : ans)
-//    {
-//        for(auto y : x)
-//        {
-//            cout << y << ' ';
-//        } cout << endl;
-//    } cout << endl;
 
     vector<string> sortedAns;
     for(int i = 0; i < ans.size(); i++)
@@ -80,44 +74,10 @@ int solution(vector<string> user_id, vector<string> banned_id) {
         sortedAns.push_back(tmp);
     }
 
-//    for(auto x : sortedAns) cout << x << endl; cout << endl;
     sort(sortedAns.begin(), sortedAns.end());
     sortedAns.erase(unique(sortedAns.begin(), sortedAns.end()), sortedAns.end());
-//    for(auto x : sortedAns) cout << x << endl;
 
 
     answer = sortedAns.size();
-//    cout << answer;
     return answer;
-}
-
-int main()
-{
-    vector<string> user_id =
-            {
-                    "frodo", "fradi", "crodo", "abc123", "frodoc"
-            };
-    vector<string> banned_id =
-            {
-                    "*rodo", "*rodo", "******"
-            };
-
-    vector<string> user_id1 =
-            {
-                    "frodo", "fradi", "crodo", "abc123", "frodoc"
-            };
-    vector<string> banned_id1 =
-            {
-                    "fr*d*", "abc1**"
-            };
-
-    vector<string> user_id3 =
-            {
-                    "frodo", "fradi", "crodo", "abc123", "frodoc"
-            };
-    vector<string> banned_id3 =
-            {
-                    "fr*d*", "*rodo", "******", "******"
-            };
-    solution(user_id1, banned_id1);
 }
