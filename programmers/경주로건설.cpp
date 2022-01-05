621b7b682f6d0bb85c3cfc9e6fdb0f36beaf9c8e
LSH3333 <dltpgustpgus@gmail.com>
2022-01-05 16:57:16 +0900

    2022.01.05

--- programmers/경주로건설.cpp
+++ programmers/경주로건설.cpp
@@ -2,48 +2,54 @@
 #include <vector>
 #include <iostream>
 #include <queue>
+#define INF 2000000000
 using namespace std;
 
 int dr[] = {-1, 0, 1, 0};
 int dc[] = {0, 1, 0, -1};
+vector<vector<int>> mark(30, vector<int>(30, INF));
 vector<vector<int>> _board;
-bool mark[26][26][4];
-
+int ans = INF;
 
 void bfs()
 {
-    queue<pair<pair<int,int>, tuple<int,int,int>>> q;
-    // {r,c}, {dir, depth, bent}
-    q.push({{0,0}, make_tuple(-1,0,0)});
+    // {r,c}, {dir,cost}
+    queue<pair<pair<int,int>, pair<int,int>>> q;
+    mark[0][0] = 0;
+    q.push({{0,0},{-1,0}});
 
     while(!q.empty())
     {
         int r = q.front().first.first;
         int c = q.front().first.second;
-        int dir = get<0>(q.front().second);
-        int depth = get<1>(q.front().second);
-        int bent = get<2>(q.front().second);
+        int dir = q.front().second.first;
+        int cost = q.front().second.second;
         q.pop();
 
         if(r == _board.size()-1 && c == _board.size()-1)
         {
-            cout << r << ' ' << c << ' ' << dir << ' ' << depth << ' ' << bent << endl;
+            ans = min(ans, cost);
         }
 
         for(int i = 0; i < 4; i++)
         {
-            if(dir != -1 && abs(dir-i) == 2) continue; // 반대로 방향으로 가지 않는다
             int nr = r + dr[i];
             int nc = c + dc[i];
             if(nr < 0 || nr >= _board.size() || nc < 0 || nc >= _board.size()) continue;
-            // 다음 지점이 벽 or 해당 방향으로 다음 지점 이동한적 있다면 continue
-            if(_board[nr][nc] == 1 || mark[nr][nc][dir]) continue;
-            int nbent = (dir == i || dir == -1) ? bent : bent+1;
-            mark[nr][nc][dir] = true;
-            q.push({{nr,nc}, make_tuple(i, depth+1, nbent)});
-        }
-    }
+            if(_board[nr][nc] == 1) continue;
+            int nCost = 0;
+            if(dir == -1 || i == dir) nCost = cost + 100;
+            else nCost = cost + 600; // 코너는 100원에 500원이 추가로 듦
 
+            // 최솟값 구해야하므로, 다음 지점이 nCost 보다 크거나 같을때만 이동함
+            if(mark[nr][nc] >= nCost)
+            {
+                mark[nr][nc] = nCost;
+                q.push({{nr,nc}, {i, nCost}});
+            }
+        }
+
+    }
 }
 
 int solution(vector<vector<int>> board) {
@@ -52,6 +58,7 @@ int solution(vector<vector<int>> board) {
 
     bfs();
 
+    answer = ans;
     return answer;
 }
 
@@ -59,14 +66,14 @@ int main()
 {
     vector<vector<int>> board2 =
             {
-                {0,0,0,0,0,0,0,1},
-                {0,0,0,0,0,0,0,0},
-                {0,0,0,0,0,1,0,0},
-                {0,0,0,0,1,0,0,0},
-                {0,0,0,1,0,0,0,1},
-                {0,0,1,0,0,0,1,0},
-                {0,1,0,0,0,1,0,0},
-                {1,0,0,0,0,0,0,0}
+                    {0,0,0,0,0,0,0,1},
+                    {0,0,0,0,0,0,0,0},
+                    {0,0,0,0,0,1,0,0},
+                    {0,0,0,0,1,0,0,0},
+                    {0,0,0,1,0,0,0,1},
+                    {0,0,1,0,0,0,1,0},
+                    {0,1,0,0,0,1,0,0},
+                    {1,0,0,0,0,0,0,0}
             };
     vector<vector<int>> board1 =
             {
@@ -81,5 +88,5 @@ int main()
                     {0,1,0,1},
                     {1,0,0,0}
             };
-    solution(board1);
+    solution(board2);
 }
