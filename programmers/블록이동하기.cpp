c85a241c62f12aba33cc6d3ff134c6c6da1c6891
LSH3333 <dltpgustpgus@gmail.com>
2022-01-25 16:15:33 +0900

    2022.01.25

--- programmers/블록이동하기.cpp
+++ programmers/블록이동하기.cpp
@@ -1,301 +1,197 @@
-#include <string>
-#include <vector>
 #include <iostream>
+#include <vector>
+#include <string>
+#include <queue>
 using namespace std;
 
-int mark[110][110][4];
-int _board[110][110];
+bool mark[110][110][4];
 int N;
 int dr[] = {-1, 0, 1, 0};
 int dc[] = {0, 1, 0, -1};
 int answer = 2000000000;
 
-int CalDir(int r1, int c1, int r2, int c2)
+struct node
 {
-    if(r1 == r2 && c2 > c1) return 1;
-    if(c1 == c2 && r1 < r2) return 2;
-    if(r1 == r2 && c1 > c2) return 3;
-    else return 0;
-}
+    int r1;
+    int c1;
+    int dir1;
 
-void dfs(int r1, int c1, int r2, int c2, int depth)
+    int r2;
+    int c2;
+    int dir2;
+
+    int depth;
+};
+
+void bfs(vector<vector<int>> &board)
 {
-    // 회전해서 온 시점에 예외 검사
-    // 범위 벗어남
-//    if(r1 <= 0 || r1 > N || c1 <= 0 || c1 > N || r2 <= 0 || r2 > N || c2 <= 0 || c2 > N) return;
-//    // 회전에서 온곳이 벽임
-//    if(_board[r1][c1] == 1 || _board[r2][c2] == 1) return;
-//    // 이미 해당 방향으로 방문한적 있음
-//    if(mark[r1][c1][CalDir(r1,c1,r2,c2)] == 1 || mark[r2][c2][CalDir(r2,c2,r1,c1)] == 1) return;
+    queue<node> q;
+    mark[0][0][1] = true;
+    mark[0][1][3] = true;
+    q.push({0, 0, 1, 0, 1, 3, 0});
 
-//    cout << r1 << ' ' << c1 << ' ' << r2 << ' ' << c2 << " depth: " << depth << endl;
-    if((r1==N && c1==N) || (r2==N && c2==N))
+    while(!q.empty())
     {
-//        cout << depth << endl;
-        answer = min(answer, depth);
-        return;
-    }
+        int r1 = q.front().r1;
+        int c1 = q.front().c1;
+        int dir1 = q.front().dir1;
+        int r2 = q.front().r2;
+        int c2 = q.front().c2;
+        int dir2 = q.front().dir2;
+        int depth = q.front().depth;
+        q.pop();
+//        cout << "depth: " << depth << endl;
+//        cout << "(" << r1 << "," << c1 << "," << dir1 << ")" << ' ';
+//        cout << "(" << r2 << "," << c2 << "," << dir2 << ")" << endl;
 
-    // 이동
-    for(int i = 0; i < 4; i++)
-    {
-        int nr1 = r1 + dr[i];
-        int nc1 = c1 + dc[i];
-        int nr2 = r2 + dr[i];
-        int nc2 = c2 + dc[i];
+        if((r1==N-1 && c1==N-1) || (r2==N-1 && c2==N-1))
+        {
+            answer = min(answer, depth);
+        }
 
-        if(nr1 <= 0 || nr1 > N || nc1 <= 0 || nc1 > N || nr2 <= 0 || nr2 > N || nc2 <= 0 || nc2 > N) continue;
-        if(_board[nr1][nc1] == 1 || _board[nr2][nc2] == 1) continue;
-        // r1,c1 기준 방향
-        int Dir1 = CalDir(nr1, nc1, nr2, nc2);
-        // r2,c2 기준 방향
-        int Dir2 = CalDir(nr2, nc2, nr1, nc1);
-        if(mark[nr1][nc1][Dir1] == 1 || mark[nr2][nc2][Dir2] == 1) continue;
-        mark[nr1][nc1][Dir1] = 1;
-        mark[nr2][nc2][Dir2] = 1;
-        dfs(nr1, nc1, nr2, nc2, depth+1);
-        mark[nr1][nc1][Dir1] = 0;
-        mark[nr2][nc2][Dir2] = 0;
-    }
+        for(int i = 0; i < 4; i++)
+        {
+            int nr1 = r1 + dr[i];
+            int nc1 = c1 + dc[i];
+            int nr2 = r2 + dr[i];
+            int nc2 = c2 + dc[i];
+            if(nr1 < 0 || nr1 >= N || nc1 < 0 || nc1 >= N ||
+               nr2 < 0 || nr2 >= N || nc2 < 0 || nc2 >= N) continue;
+            if(mark[nr1][nc1][dir1] || mark[nr2][nc2][dir2]) continue;
+            if(board[nr1][nc1] == 1 || board[nr2][nc2] == 1) continue;
+            mark[nr1][nc1][dir1] = true;
+            mark[nr2][nc2][dir2] = true;
+            q.push({nr1, nc1, dir1, nr2, nc2, dir2, depth+1});
+        }
 
-    // 회전
-    // r1,c1이 좌에서 우로
-    if(CalDir(r1,c1,r2,c2) == 1)
-    {
-        // r1,c1 기준 시계
-        if(r1+1 <= N && _board[r1+1][c1+1] == 0 && _board[r1+1][c1] == 0)
+        // 좌우 방향인 경우
+        if(dir1 == 1 || dir1 == 3)
         {
-            if(mark[r1][c1][CalDir(r1,c1,r2+1,c2-1)] == 0)
+            int left_r, left_c, right_r, right_c;
+            if(c1 < c2) {left_r = r1; left_c = c1; right_r = r2; right_c = c2;}
+            else {left_r = r2; left_c = c2; right_r = r1; right_c = c1;}
+            // 왼쪽 기준 시계방향 회전
+            if(left_r+1 < N && board[left_r+1][left_c] == 0 && board[left_r+1][left_c+1] == 0
+               && !mark[left_r][left_c][2] && !mark[right_r+1][right_c-1][0])
             {
-                mark[r1][c1][CalDir(r1,c1,r2+1,c2-1)] = 1;
-                mark[r2+1][c2-1][CalDir(r2+1,c2-1,r1,c1)] = 1;
-                dfs(r1, c1, r2+1, c2-1, depth+1);
-                mark[r1][c1][CalDir(r1,c1,r2+1,c2-1)] = 0;
-                mark[r2+1][c2-1][CalDir(r2+1,c2-1,r1,c1)] = 0;
+                q.push({left_r, left_c, 2, right_r+1, right_c-1, 0, depth+1});
+                mark[left_r][left_c][2] = true;
+                mark[right_r+1][right_c-1][0] = true;
             }
+
+            // 왼쪽 기준 반시계방향 회전
+            if(left_r-1 >= 0 && board[left_r-1][c1] == 0 && board[left_r-1][left_c+1] == 0
+               && !mark[left_r][left_c][0] && !mark[right_r-1][right_c-1][2])
+            {
+                q.push({left_r, left_c, 0, right_r-1, right_c-1, 2, depth+1});
+                mark[left_r][left_c][0] = true;
+                mark[right_r-1][right_c-1][2] = true;
+            }
+
+            // 오른쪽 기준 시계방향 회전
+            if(right_r-1 >= 0 && board[right_r-1][right_c] == 0 && board[right_r-1][right_c-1] == 0
+               && !mark[right_r][right_c][0] && !mark[left_r-1][left_c+1][2])
+            {
+                q.push({left_r-1, left_c+1, 2, right_r, right_c, 0, depth+1});
+                mark[left_r-1][left_c+1][2] = true;
+                mark[right_r][right_c][0] = true;
+            }
+
+            // 오른쪽 기준 반시계방향 회전
+            if(right_r+1 < N && board[right_r+1][right_c] == 0 && board[right_r+1][right_c-1] == 0
+               && !mark[right_r][right_c][2] && !mark[left_r+1][left_c+1][0])
+            {
+                q.push({left_r+1, left_c+1, 0, right_r, right_c, 2, depth+1});
+                mark[left_r+1][left_c+1][0] = true;
+                mark[right_r][right_c][2] = true;
+            }
+
         }
-        // r1,c1 기준 반시계
-        if(r1-1 > 0 && _board[r1-1][c1+1] == 0 && _board[r1-1][c1] == 0)
+            // 상하 방향인 경우
+        else
         {
-            if(mark[r1][c1][CalDir(r1,c1,r2-1,c2-1)] == 0)
+            int up_r, up_c, down_r, down_c;
+            if(r1 < r2) {up_r = r1; up_c = c1; down_r = r2; down_c = c2;}
+            else {up_r = r2; up_c = c2; down_r = r1; down_c = c1;}
+
+            // 위 기준 시계 방향 회전
+            if(up_c-1 >= 0 && board[up_r][up_c-1] == 0 && board[up_r+1][up_c-1] == 0
+               && !mark[up_r][up_c][3] && !mark[down_r-1][down_c-1][1])
             {
-                mark[r1][c1][CalDir(r1,c1,r2-1,c2-1)] = 1;
-                mark[r2-1][c2-1][CalDir(r2-1,c2-1,r1,c1)] = 1;
-                dfs(r1, c1, r2-1, c2-1, depth+1);
-                mark[r1][c1][CalDir(r1,c1,r2-1,c2-1)] = 0;
-                mark[r2-1][c2-1][CalDir(r2-1,c2-1,r1,c1)] = 0;
+                q.push({up_r, up_c, 3, down_r-1, down_c-1, 1, depth+1});
+                mark[up_r][up_c][3] = true;
+                mark[down_r-1][down_c-1][1] = true;
             }
-        }
-        // r2,c2 기준 시계
-        if(r2-1 > 0 && _board[r2-1][c2] == 0 && _board[r2-1][c2-1] == 0)
-        {
-            if(mark[r1-1][c1+1][CalDir(r1-1,c1+1,r2,c2)] == 0)
+
+            // 위 기준 반시계 방향 회전
+            if(up_c+1 < N && board[up_r][up_c+1] == 0 && board[up_r+1][up_c+1] == 0
+               && !mark[up_r][up_c][1] && !mark[down_r-1][down_c+1][3])
             {
-                mark[r1-1][c1+1][CalDir(r1-1,c1+1,r2,c2)] = 1;
-                mark[r2][c2][CalDir(r2,c2,r1-1,c1+1)] = 1;
-                dfs(r1-1, c1+1, r2, c2, depth+1);
-                mark[r1-1][c1+1][CalDir(r1-1,c1+1,r2,c2)] = 0;
-                mark[r2][c2][CalDir(r2,c2,r1-1,c1+1)] = 0;
+                q.push({up_r, up_c, 1, down_r-1, down_c+1, 3, depth+1});
+                mark[up_r][up_c][1] = true;
+                mark[down_r-1][down_c+1][3] = true;
             }
-        }
-        // r2,c2 기준 반시계
-        if(r1+1 <= N && _board[r2+1][c2] == 0 && _board[r2+1][c2-1] == 0)
-        {
-            if(mark[r1+1][c1+1][CalDir(r1+1,c1+1,r2,c2)] == 0)
+
+            // 아래 기준 시계 방향 회전
+            if(down_c+1 < N && board[down_r][down_c+1] == 0 && board[down_r-1][down_c+1] == 0
+               && !mark[down_r][down_c][1] && !mark[up_r+1][up_c+1][3])
             {
-                mark[r1+1][c1+1][CalDir(r1+1,c1+1,r2,c2)] = 1;
-                mark[r2][c2][CalDir(r2,c2,r1+1,c1+1)] = 1;
-                dfs(r1+1, c1+1, r2, c2, depth+1);
-                mark[r1+1][c1+1][CalDir(r1+1,c1+1,r2,c2)] = 0;
-                mark[r2][c2][CalDir(r2,c2,r1+1,c1+1)] = 0;
+                q.push({up_r+1, up_c+1, 3, down_r, down_c, 1, depth+1});
+                mark[up_r+1][up_c+1][3] = true;
+                mark[down_r][down_c][1] = true;
             }
-        }
-    }
-        // r1,c1이 우에서 좌로
-    else if(CalDir(r1,c1,r2,c2) == 3)
-    {
-        // r1,c1 기준 시계
-        if(r1-1 > 0 && _board[r1-1][c1] == 0 && _board[r1-1][c1-1] == 0)
-        {
-            if(mark[r1][c1][CalDir(r1,c1,r2-1,c2+1)] == 0)
+
+            // 아래 기준 반시계 방향 회전
+            if(down_c-1 >= 0 && board[down_r][down_c-1] == 0 && board[down_r-1][down_c-1] == 0
+               && !mark[down_r][down_c][3] && !mark[up_r+1][up_c-1][1])
             {
-                mark[r1][c1][CalDir(r1,c1,r2-1,c2+1)] = 1;
-                mark[r2-1][c2+1][CalDir(r2-1,c2+1,r1,c1)] = 1;
-                dfs(r1, c1, r2-1, c2+1, depth+1);
-                mark[r1][c1][CalDir(r1,c1,r2-1,c2+1)] = 0;
-                mark[r2-1][c2+1][CalDir(r2-1,c2+1,r1,c1)] = 0;
+                q.push({up_r+1, up_c-1, 1, down_r, down_c, 3, depth+1});
+                mark[up_r+1][up_c-1][1] = true;
+                mark[down_r][down_c][3] = true;
             }
-        }
-        // r1,c1 기준 반시계
-        if(r1+1 <= N && _board[r1+1][c1] == 0 && _board[r1+1][c1-1] == 0)
-        {
-            if(mark[r1][c1][CalDir(r1,c1,r2+1,c2+1)] == 0)
-            {
-                mark[r1][c1][CalDir(r1,c1,r2+1,c2+1)] = 1;
-                mark[r2+1][c2+1][CalDir(r2+1,c2+1,r1,c1)] = 1;
-                dfs(r1, c1, r2+1, c2+1, depth+1);
-                mark[r1][c1][CalDir(r1,c1,r2+1,c2+1)] = 0;
-                mark[r2+1][c2+1][CalDir(r2+1,c2+1,r1,c1)] = 0;
-            }
-        }
-        // r2,c2 기준 시계
-        if(r2+1 <= N && _board[r2+1][c2] == 0 && _board[r2+1][c2+1] == 0)
-        {
-            if(mark[r1+1][c1-1][CalDir(r1+1,c1-1,r2,c2)] == 0)
-            {
-                mark[r1+1][c1-1][CalDir(r1+1,c1-1,r2,c2)] = 1;
-                mark[r2][c2][CalDir(r2,c2,r1+1,c1-1)] = 1;
-                dfs(r1+1, c1-1, r2, c2, depth+1);
-                mark[r1+1][c1-1][CalDir(r1+1,c1-1,r2,c2)] = 0;
-                mark[r2][c2][CalDir(r2,c2,r1+1,c1-1)] = 0;
-            }
-        }
-        // r2,c2 기준 반시계
-        if(r2-1 > 0 && _board[r2-1][c2] == 0 && _board[r2-1][c2+1] == 0)
-        {
-            if(mark[r1-1][c1-1][CalDir(r1-1,c1-1,r2,c2)] == 0)
-            {
-                mark[r1-1][c1-1][CalDir(r1-1,c1-1,r2,c2)] = 1;
-                mark[r2][c2][CalDir(r2,c2,r1-1,c1-1)] = 1;
-                dfs(r1-1, c1-1, r2, c2, depth+1);
-                mark[r1-1][c1-1][CalDir(r1-1,c1-1,r2,c2)] = 0;
-                mark[r2][c2][CalDir(r2,c2,r1-1,c1-1)] = 0;
-            }
-        }
-    }
-        // r1,c1이 위에서 아래로
-    else if(CalDir(r1,c1,r2,c2) == 2)
-    {
-        // r1,c1 기준 시계
-        if(c1-1 > 0 && _board[r1][c1-1] == 0 && _board[r1+1][c1-1] == 0)
-        {
-            if(mark[r1][c1][CalDir(r1,c1,r2-1,c2-1)] == 0)
-            {
-                mark[r1][c1][CalDir(r1,c1,r2-1,c2-1)] = 1;
-                mark[r2-1][c2-1][CalDir(r2-1,c2-1,r1,c1)] = 1;
-                dfs(r1, c1, r2-1, c2-1, depth+1);
-                mark[r1][c1][CalDir(r1,c1,r2-1,c2-1)] = 0;
-                mark[r2-1][c2-1][CalDir(r2-1,c2-1,r1,c1)] = 0;
-            }
-        }
-        // r1,c1 기준 반시계
-        if(c1+1 <= N && _board[r1][c1+1] == 0 && _board[r1+1][c1+1] == 0)
-        {
-            if(mark[r1][c1][CalDir(r1,c1,r2-1,c2+1)] == 0)
-            {
-                mark[r1][c1][CalDir(r1,c1,r2-1,c2+1)] = 1;
-                mark[r2-1][c2+1][CalDir(r2-1,c2+1,r1,c1)] = 1;
-                dfs(r1, c1, r2-1, c2+1, depth+1);
-                mark[r1][c1][CalDir(r1,c1,r2-1,c2+1)] = 0;
-                mark[r2-1][c2+1][CalDir(r2-1,c2+1,r1,c1)] = 10;
-            }
-        }
-        // r2,c2 기준 시계
-        if(c2+1 <= N && _board[r2][c2+1] == 0 && _board[r2-1][c2+1] == 0)
-        {
-            if(mark[r1+1][c1+1][CalDir(r1+1,c1+1,r2,c2)] == 0)
-            {
-                mark[r1+1][c1+1][CalDir(r1+1,c1+1,r2,c2)] = 1;
-                mark[r2][c2][CalDir(r2,c2,r1+1,c1+1)] = 1;
-                dfs(r1+1, c1+1, r2, c2, depth+1);
-                mark[r1+1][c1+1][CalDir(r1+1,c1+1,r2,c2)] = 0;
-                mark[r2][c2][CalDir(r2,c2,r1+1,c1+1)] = 0;
-            }
-        }
-        // r2,c2 기준 반시계
-        if(c2-1 > 0 && _board[r2][c2-1] == 0 && _board[r2-1][c2-1] == 0)
-        {
-            if(mark[r1+1][c1-1][CalDir(r1+1,c1-1,r2,c2)] == 0)
-            {
-                mark[r1+1][c1-1][CalDir(r1+1,c1-1,r2,c2)] = 1;
-                mark[r2][c2][CalDir(r2,c2,r1+1,c1-1)] = 1;
-                dfs(r1+1, c1-1, r2, c2, depth+1);
-                mark[r1+1][c1-1][CalDir(r1+1,c1-1,r2,c2)] = 0;
-                mark[r2][c2][CalDir(r2,c2,r1+1,c1-1)] = 0;
-            }
-        }
-    }
-        // r1,c1이 아래서 위로
-    else
-    {
-        // r1,c1 기준 시계
-        if(c1+1 <= N && _board[r1][c1+1] == 0 && _board[r1-1][c1+1] == 0)
-        {
-            if(mark[r1][c1][CalDir(r1,c1,r2+1,c2+1)] == 0)
-            {
-                mark[r1][c1][CalDir(r1,c1,r2+1,c2+1)] = 1;
-                mark[r2+1][c2+1][CalDir(r2+1,c2+1,r1,c1)] = 1;
-                dfs(r1, c1, r2+1, c2+1, depth+1);
-                mark[r1][c1][CalDir(r1,c1,r2+1,c2+1)] = 0;
-                mark[r2+1][c2+1][CalDir(r2+1,c2+1,r1,c1)] = 0;
-            }
-        }
-        // r1,c1 기준 반시계
-        if(c1-1 > 0 && _board[r1][c1-1] == 0 && _board[r1-1][c1-1] == 0)
-        {
-            if(mark[r1][c1][CalDir(r1,c1,r2+1,c2-1)] == 0)
-            {
-                mark[r1][c1][CalDir(r1,c1,r2+1,c2-1)] = 1;
-                mark[r2+1][c2-1][CalDir(r2+1,c2-1,r1,c1)] = 1;
-                dfs(r1, c1, r2+1, c2-1, depth+1);
-                mark[r1][c1][CalDir(r1,c1,r2+1,c2-1)] = 0;
-                mark[r2+1][c2-1][CalDir(r2+1,c2-1,r1,c1)] = 0;
-            }
-        }
-        // r2,c2 기준 시계
-        if(c2-1 > 0 && _board[r2][c2-1] && _board[r2+1][c2-1] == 0)
-        {
-            if(mark[r1-1][c1-1][CalDir(r1-1,c1-1,r2,c2)] == 0)
-            {
-                mark[r1-1][c1-1][CalDir(r1-1,c1-1,r2,c2)] = 1;
-                mark[r2][c2][CalDir(r2,c2,r1-1,c1-1)] = 1;
-                dfs(r1-1, c1-1, r2, c2, depth+1);
-                mark[r1-1][c1-1][CalDir(r1-1,c1-1,r2,c2)] = 0;
-                mark[r2][c2][CalDir(r2,c2,r1-1,c1-1)] = 0;
-            }
-        }
-        // r2,c2 기준 반시계
-        if(c2+1 <= N && _board[r2][c2+1] == 0 && _board[r2+1][c2+1] == 0)
-        {
-            if(mark[r1-1][c1+1][CalDir(r1-1,c1+1,r2,c2)] == 0)
-            {
-                mark[r1-1][c1+1][CalDir(r1-1,c1+1,r2,c2)] = 1;
-                mark[r2][c2][CalDir(r2,c2,r1-1,c1+1)] = 1;
-                dfs(r1-1, c1+1, r2, c2, depth+1);
-                mark[r1-1][c1+1][CalDir(r1-1,c1+1,r2,c2)] = 0;
-                mark[r2][c2][CalDir(r2,c2,r1-1,c1+1)] = 0;
-            }
+
         }
     }
 }
-
-int solution(vector<vector<int>> board) {
+int solution(vector<vector<int>> board)
+{
     N = board.size();
 
-    for(int i = 0; i < board.size(); i++)
-    {
-        for(int j = 0; j < board.size(); j++)
-        {
-            _board[i+1][j+1] = board[i][j];
-        }
-    }
-    mark[1][1][1] = 1;
-    mark[1][2][3] = 1;
+    bfs(board);
 
-    dfs(1, 1, 1, 2, 0);
 //    cout << answer;
     return answer;
 }
 
 int main()
 {
+//    vector<vector<int>> board =
+//            {
+//                    {0,0,0,1,1},
+//                    {0,0,0,1,0},
+//                    {0,1,0,1,1},
+//                    {1,1,0,0,1},
+//                    {0,0,0,0,0}
+//            };
+//    solution(board);
+
+//    vector<vector<int>> board =
+//            {
+//                    {0, 0, 0, 0, 1, 0},
+//                    {0, 0, 1, 1, 1, 0},
+//                    {0, 1, 1, 1, 1, 0},
+//                    {0, 1, 0, 0, 1, 0},
+//                    {0, 0, 1, 0, 0, 0},
+//                    {0, 0, 0, 0, 0, 0}
+//            };
+//    solution(board);
     vector<vector<int>> board =
             {
-                    {0,0,0,1,1},
-                    {0,0,0,1,0},
-                    {0,1,0,1,1},
-                    {1,1,0,0,1},
-                    {0,0,0,0,0}
+                    {0, 0, 0, 0, 0},
+                    {0, 0, 0, 0, 0},
+                    {0, 0, 0, 0, 0},
+                    {0, 0, 0, 0, 0},
+                    {0, 0, 0, 0, 0}
             };
     solution(board);
 }
