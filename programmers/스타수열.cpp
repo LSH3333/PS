860452e8bd9ab08f40452a92c375318e2b68a9e2
LSH3333 <dltpgustpgus@gmail.com>
2022-02-04 15:04:06 +0900

    2022.02.04

--- programmers/스타수열.cpp
+++ programmers/스타수열.cpp
@@ -5,56 +5,48 @@
 #include <algorithm>
 using namespace std;
 
-
-
 int solution(std::vector<int> a) {
-    int answer = -1;
+    int answer = 0;
     unordered_map<int,int> map;
 
     for(auto x : a) map[x]++;
     vector<pair<int,int>> v;
-    for(auto x : map)
-    {
-        v.push_back({x.second, x.first});
-    }
+    for(auto x : map) v.push_back({x.second, x.first});
 
     sort(v.begin(), v.end(), greater<>());
-
-//    for(auto x : v)
-//    {
-//        cout << x.first << ' '  << x.second << endl;
-//    }
 
     for(int i = 0; i < v.size(); i++)
     {
         int target = v[i].second;
-        if(v[i].first < answer) continue;
-
+        if(v[i].first * 2 < answer) break;
         int cnt = 0;
-        cout << "target: " << target << endl;
+        int chose = -1;
         for(int j = 0; j < a.size(); j++)
         {
             if(a[j] == target)
             {
-                if(j-1 >= 0 && a[j-1] != target)
+                if(j-1 >= 0 && a[j-1] != target && chose != j-1)
                 {
+                    chose = j-1;
                     cnt++;
                 }
-                else if(j+1 < a.size() && a[j+1] != target)
+                else if(j+1 < a.size() && a[j+1] != target && chose != j+1)
                 {
+                    chose = j+1;
                     cnt++;
-                    i++;
+                    j++;
                 }
             }
         }
-        cout << cnt << endl;
         // target을 각 집합에 포함하는 스타수열이 존재함
         if(cnt > 0)
         {
             answer = max(answer, cnt*2);
         }
+        cout << "answer: " << answer << endl;
     }
 
+    cout << endl;
     cout << "answer: " << answer;
     return answer;
 }
@@ -63,5 +55,6 @@ int main()
 {
     vector<int> a = {0,3,3,0,7,2,0,2,2,0};
     vector<int> a1 = {5,2,3,3,5,3};
-    solution(a1);
+    vector<int> a2 = {4, 0, 0, 2, 1, 1, 1, 1, 1, 1, 1, 1, 0, 3};
+    solution(a2);
 }
