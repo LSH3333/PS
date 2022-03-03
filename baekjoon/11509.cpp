b4a0186dface45cfe3830b21b7a2d18aa2d93faf
LSH3333 <dltpgustpgus@gmail.com>
2022-03-03 15:25:12 +0900

    2022.03.03

--- baekjoon/11509.cpp
+++ baekjoon/11509.cpp
@@ -1,40 +1,55 @@
 #include <iostream>
-#define MAX 1000001
+#include <vector>
+#include <algorithm>
+#include <deque>
 using namespace std;
 
-int n;
-int balloon[MAX];
-int mark[MAX];
+bool cmp(const pair<int,int> &a, const pair<int,int> &b)
+{
+    return a.first < b.first;
+}
+
+bool mark[1000001];
 
 int main()
 {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL); cout.tie(NULL);
-
-    cin >> n;
-    for(int i = 0; i < n; i++)
-        cin >> balloon[i];
-
+    ios::sync_with_stdio(false); cin.tie(NULL);
+    int n; cin >> n;
+    // num, idx
+    vector<pair<int,int>> v(n);
+//    deque<pair<int,int>> v(n);
     for(int i = 0; i < n; i++)
     {
-        int h = balloon[i];
+        int num; cin >> num;
+        v[i] = {num * -1, i};
+    }
 
-        if(mark[h+1] > 0)
+    sort(v.begin(), v.end(), less<>());
+
+//    for(int i = 0; i < n; i++) cout << v[i].first << ' ' << v[i].second << endl; cout << endl;
+    int answer = 0;
+    while(!v.empty())
+    {
+        answer++;
+        int h = v.front().first + 1;
+        int idx = v.front().second + 1;
+        v.erase(v.begin());
+
+        auto it = v.begin();
+        while(it != v.end())
         {
-            mark[h+1]--;
-            mark[h]++;
-        }
-        else
-        {
-            mark[h]++;
+            it = lower_bound(it, v.end(), pair<int,int>(h,0), cmp);
+            if(it == v.end()) break;
+            if(it->first != h) break;
+            if(it->second >= idx)
+            {
+                h = it->first + 1;
+                idx = it->second + 1;
+                it = v.erase(it);
+            }
+            else it++;
         }
     }
 
-    int ans = 0;
-    for(int i = 1; i <= n; i++)
-    {
-        ans += mark[i];
-    }
-
-    cout << ans;
+    cout << answer;
 }
