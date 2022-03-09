d93715e8a7d716d4e78939928a5cf314c9f47f19
LSH <dltpgustpgus@gmail.com>
2022-03-09 21:28:19 +0900

    2022.03.09

--- baekjoon/1300 (ing).cpp
+++ baekjoon/1300 (ing).cpp
@@ -2,48 +2,39 @@
 #include <algorithm>
 using namespace std;
 
-int n, k;
+long long n, k;
 
-bool Count(long long mid)
+long long Count(long long mid)
 {
-    // mid보다 작은 숫자 갯수
+    // mid 보다 작거나 같은 숫자 갯수
     long long cnt = 0;
-    for(int i = 1; i <= n; i++)
+    for(long long i = 1; i <= n; i++)
     {
-        if(i * n < mid)
+        if(i * n <= mid)
             cnt += n;
-        else if(i * n == mid)
-            cnt += n-1;
         else
-        {
-            if(mid % i == 0) cnt += mid / i - 1;
-            else cnt += mid / i;
-        }
+            cnt += mid / i;
     }
-//    cout << "cnt: " << cnt << endl;
-    // mid 보다 작거나 같은 값이 k개 이하로 존재
-    if(cnt < k) return true;
-    return false;
+    return cnt;
 }
 
 int main()
 {
     cin >> n >> k;
-    long long left = 1, right = n * n;
-    long long ans = 0;
+    long long left = 1, right = n*n;
+    long long ans = (left + right) / 2;
     while(left <= right)
     {
         long long mid = (left + right) / 2;
-//        cout << left << ' ' << mid << ' ' << right << endl;
-        if(Count(mid))
+
+        long long cnt = Count(mid);
+        if(cnt < k)
         {
-//            cout << "T" << endl;
             left = mid + 1;
-            ans = mid;
         }
         else
         {
-//            cout << "F" << endl;
+            ans = mid;
             right = mid - 1;
         }
     }
