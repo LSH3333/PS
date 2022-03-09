47e3b4d02880b0943ba4f6b086d44513751ed812
LSH3333 <dltpgustpgus@gmail.com>
2022-03-09 15:26:41 +0900

    2022.03.09

---
+++ baekjoon/12738.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int n;
+vector<int> v;
+vector<int> ans;
+
+int main()
+{
+    ios::sync_with_stdio(false); cin.tie(NULL);
+
+    cin >> n;
+    for(int i = 0; i < n; i++)
+    {
+        int num; cin >> num;
+        v.push_back(num);
+    }
+
+    ans.push_back(-2000000000);
+    int cnt = 0;
+    for(int i = 0; i < n; i++)
+    {
+        int num = v[i];
+        if(num > ans.back())
+        {
+            ans.push_back(num); cnt++;
+        }
+        else
+        {
+            int idx = lower_bound(ans.begin(), ans.end(), num) - ans.begin();
+            ans[idx] = num;
+        }
+    }
+
+    cout << ans.size()-1;
+}
