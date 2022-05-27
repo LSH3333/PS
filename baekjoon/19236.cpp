d13fadbd56ce873a38c7f690c968fa2cb5d2dad5
LSH3333 <dltpgustpgus@gmail.com>
2022-05-27 15:37:49 +0900

    2022.05.27

--- baekjoon/19236.cpp
+++ baekjoon/19236.cpp
@@ -1,159 +1,182 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
 using namespace std;
 
 int dr[] = {-1, -1, 0, 1, 1, 1, 0, -1};
 int dc[] = {0, -1, -1, -1, 0, 1, 1, 1};
-
 int answer;
 
-class Fish
+struct Fish
 {
-public:
-    int num;
-    int dir;
-    int r;
-    int c;
-    Fish(int _num, int _dir, int _r, int _c): num(_num), dir(_dir), r(_r), c(_c) {}
-};
-
-class Shark
-{
-public:
     int r;
     int c;
     int dir;
-    Shark(int _r, int _c, int _dir) : r(_r), c(_c), dir(_dir) {}
+    bool alive;
 };
 
-void Print(const vector<Fish> &loc, const Shark& shark, const vector<vector<int>> &board)
+struct Shark
+{
+    int r;
+    int c;
+    int dir;
+};
+
+// 0: 빈공간, -1: shark
+int board[4][4];
+
+void Print(Fish fishes[], Shark &shark)
 {
     for(int i = 0; i < 4; i++)
     {
         for(int j = 0; j < 4; j++)
         {
-            if(board[i][j] == -2) cout << board[i][j] << ',' << shark.dir << ' '; // 상어
-            else
-            {
-                if(loc[board[i][j]].num == -1) cout << 0 << ',' << 0 << ' '; // 빈 공간
-                else cout << board[i][j] << ',' << loc[board[i][j]].dir << ' ';
-            }
+            if(board[i][j] == -1) { cout << board[i][j] << ',' << shark.dir << ' '; }
+            else { cout << board[i][j] << ',' << fishes[board[i][j]].dir << ' ';}
         } cout << endl;
     } cout << endl;
 }
 
-void MoveFish(Fish &fish, Shark &shark, vector<Fish> &loc, vector<vector<int>> &board)
+// fishNum 번호의 물고기 이동
+void MoveFish(int fishNum, Fish fishes[])
 {
-    int dir = fish.dir;
+    int dir = fishes[fishNum].dir;
     for(int i = 0; i < 8; i++, dir = (dir+1)%8)
     {
-        int nr = fish.r + dr[dir];
-        int nc = fish.c + dc[dir];
+        int nr = fishes[fishNum].r + dr[dir];
+        int nc = fishes[fishNum].c + dc[dir];
 
         if(nr < 0 || nr >= 4 || nc < 0 || nc >= 4) continue;
-        if(board[nr][nc] == -2) continue; // shark
+        if(board[nr][nc] == -1) continue;
 
-        // 이동, 자리 바꿈
-        Fish cur(fish.num, dir, fish.r, fish.c);
-        Fish target(loc[board[nr][nc]].num, loc[board[nr][nc]].dir, loc[board[nr][nc]].r, loc[board[nr][nc]].c);
-        // 위치 정보 swap
-        loc[cur.num] = {cur.num, cur.dir, target.r, target.c};
-        loc[target.num] = {target.num, target.dir, cur.r, cur.c};
+        if(board[nr][nc] == 0) // 빈 자리
+        {
+            board[fishes[fishNum].r][fishes[fishNum].c] = 0;
+            board[nr][nc] = fishNum;
 
-        board[cur.r][cur.c] = board[nr][nc];
-        board[nr][nc] = cur.num;
+            fishes[fishNum].dir = dir;
+            fishes[fishNum].r = nr;
+            fishes[fishNum].c = nc;
+        }
+        else
+        {
+            // 자리 변경
+            Fish tmp = fishes[fishNum];
+            int otherFishNum = board[nr][nc];
+            fishes[fishNum].r = fishes[otherFishNum].r;
+            fishes[fishNum].c = fishes[otherFishNum].c;
+            fishes[fishNum].dir = dir;
+            fishes[otherFishNum].r = tmp.r;
+            fishes[otherFishNum].c = tmp.c;
+
+            board[fishes[fishNum].r][fishes[fishNum].c] = fishNum;
+            board[fishes[otherFishNum].r][fishes[otherFishNum].c] = otherFishNum;
+
+        }
 
         break;
     }
 }
 
-// 모든 물고기들의 이동
-void MoveAllFishes(vector<Fish> &loc, Shark &shark, vector<vector<int>> &board)
+void MoveAllFishes(Fish fishes[])
 {
     for(int i = 1; i <= 16; i++)
     {
-        if(loc[i].num == -1) continue; // 잡아 먹힌 물고기
-        MoveFish(loc[i], shark, loc, board);
+        if(!fishes[i].alive) continue; // 이미 먹힌 물고기
+        MoveFish(i, fishes);
     }
 }
 
-// board[r][c]의 물고기를 먹음, 먹은 물고기의 수 리턴
-int EatFish(Shark &shark, vector<Fish> &loc, int r, int c, vector<vector<int>> &board)
+// shark가 [r][c]에 있는 물고기를 먹음
+void SharkEatFish(Shark &shark, Fish fishes[], int r, int c)
 {
-    int ate = board[r][c];
-    Shark before = shark;
-    // 상어 이동
-    board[r][c] = -2;
-    shark.r = r; shark.c = c;
-    shark.dir = loc[ate].dir;
-    // 먹힌 물고기 처리
-    loc[ate].num = -1; loc[ate].dir = -1; loc[ate].r = -1; loc[ate].c = -1;
-    board[before.r][before.c] = 0;
-
-    return ate;
+    int fishNum = board[r][c];
+    shark.r = r;
+    shark.c = c;
+    shark.dir = fishes[fishNum].dir;
+    fishes[fishNum].alive = false; // fish 먹힘
+    board[r][c] = -1;
 }
 
-void dfs(Shark shark, vector<Fish> &loc, int ate, vector<vector<int>> &board)
+void Copy(int SaveBoard[][4], Fish SaveFishes[], Fish fishes[])
 {
-    answer = max(answer, ate);
-    // back up
-    vector<vector<int>> resBoard = board;
-    vector<Fish> restoreLoc = loc;
-
-    cout << "ate: " << ate << endl;
-    Print(loc, shark, board);
-
-    MoveAllFishes(loc, shark, board);
-
-    int r = shark.r, c = shark.c;
-    for(int i = 0; i <= 3; i++)
+    // board
+    for(int i = 0; i < 4; i++)
     {
-        r = r + dr[shark.dir];
-        c = c + dc[shark.dir];
-        if(r < 0 || r >= 4 || c < 0 || c >= 4) break;
-        if(board[r][c] == 0) continue;
-
-        int amount = EatFish(shark, loc, r, c, board);
-
-        dfs(shark, loc, ate+amount, board);
-
-        resBoard = board;
-        loc = restoreLoc;
+        for(int j = 0; j < 4; j++)
+        {
+            SaveBoard[i][j] = board[i][j];
+        }
     }
 
-    resBoard = board;
-    loc = restoreLoc;
+    // fishes
+    for(int i = 0; i < 17; i++) SaveFishes[i] = fishes[i];
+}
+
+void Paste(int SaveBoard[][4], Fish SaveFishes[], Fish fishes[])
+{
+    for(int i = 0; i < 4; i++)
+    {
+        for(int j = 0; j < 4; j++)
+        {
+            board[i][j] = SaveBoard[i][j];
+        }
+    }
+    for(int i = 0; i < 17; i++) fishes[i] = SaveFishes[i];
+}
+
+void dfs(int r, int c, int dir, int sum, Fish fishes[])
+{
+    answer = max(answer, sum);
+    
+    MoveAllFishes(fishes);
+    
+    int nr = r; int nc = c;
+    for(int i = 0; i < 3; i++)
+    {
+        nr = nr + dr[dir];
+        nc = nc + dc[dir];
+        if(nr < 0 || nr >= 4 || nc < 0 || nc >= 4) break;
+        if(board[nr][nc] == 0) continue;
+
+        int fish = board[nr][nc];
+        int nextDir = fishes[fish].dir;
+
+        // save
+        int SaveBoard[4][4];
+        Fish SaveFishes[20];
+        Copy(SaveBoard, SaveFishes, fishes);
+        
+        // 상어 이동 후 물고기 잡아먹음 
+        fishes[fish].alive = false;
+        board[r][c] = 0;
+        board[nr][nc] = -1;
+
+        dfs(nr,nc, nextDir, sum+fish, fishes);
+
+        // 복구 
+        Paste(SaveBoard, SaveFishes, fishes);
+    }
+
 }
 
 int main()
 {
-    // 물고기들의 위치
-    vector<Fish> loc(17, {-1,-1,-1,-1});
-    vector<vector<int>> board(4, vector<int>(4,0));
+    Fish fishes[17];
+
     for(int i = 0; i < 4; i++)
     {
         for(int j = 0; j < 4; j++)
         {
             int num, dir; cin >> num >> dir;
             board[i][j] = num;
-            loc[num] = Fish(num, dir-1, i, j);
+            fishes[num] = {i, j, dir-1, true};
         }
     }
-    loc[0] = {0, 0, 0, 0};
 
+    int f = board[0][0];
+    Shark shark = {0, 0, 0};
+    SharkEatFish(shark, fishes, 0, 0);
 
-    int firstFish = board[0][0];
-    Shark shark(0,0,0);
-    // (0,0)에 상어 투입
-    shark.dir = loc[firstFish].dir;
-    loc[board[0][0]] = {-1,-1,-1,-1};
-    board[0][0] = -2;
-
-
-    Print(loc, shark, board);
-
-    dfs(shark, loc, firstFish, board);
+    dfs(shark.r, shark.c, shark.dir, f, fishes);
 
     cout << answer;
 }
