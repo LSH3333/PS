94a94ed96138023c5d733f4542a15152e82309b9
LSH3333 <dltpgustpgus@gmail.com>
2022-07-30 17:00:51 +0900

    2022.07.30

---
+++ works/10998.dart
@@ -0,0 +1,11 @@
+import 'dart:io';
+
+void main()
+{
+    String? input = stdin.readLineSync(); // return String
+    // input은 nullable이므로 !로 null이 아님을 나타내 줘야 함
+    var list = input!.split(' ');
+    int A = int.parse(list[0]);
+    int B = int.parse(list[1]);
+    print(A*B);
+}
---
+++ works/2558.dart.txt
@@ -0,0 +1,8 @@
+import 'dart:io';
+
+void main()
+{
+    var A = int.parse(stdin.readLineSync()!);
+    var B = int.parse(stdin.readLineSync()!);
+    print(A+B);
+}
