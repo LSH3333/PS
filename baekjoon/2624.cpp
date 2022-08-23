f0456845376bce5180b5b88ff63d80ac9c2b86d8
LSH3333 <dltpgustpgus@gmail.com>
2022-08-23 14:32:06 +0900

    2022.08.23

--- baekjoon/2624.cpp
+++ baekjoon/2624.cpp
@@ -1,57 +1,49 @@
 #include <iostream>
-#include <vector>
 #include <algorithm>
+#include <cstring>
 using namespace std;
 
 int d[101][10001];
+int T, K;
 int coin[101]; // 동전의 종류
 int coinNum[101]; // 동전의 갯수
+
+int dp(int amount, int idx)
+{
+    if(amount == T) // 조건 만족
+    {
+        return 1;
+    }
+    if(idx == K || amount > T) // 조건 벗어남
+    {
+        return 0;
+    }
+    if(d[idx][amount] != -1)
+    {
+        return d[idx][amount];
+    }
+
+    int res = 0;
+    for(int i = 0; i <= coinNum[idx]; i++)
+    {
+        int cost = coin[idx] * i;
+        res += dp(amount + cost, idx + 1);
+    }
+
+    return d[idx][amount] = res;
+}
 
 int main()
 {
     ios::sync_with_stdio(false); cin.tie(NULL);
-    int T, K; cin >> T >> K;
-    vector<pair<int,int>> v(K);
+    cin >> T >> K;
     for(int i = 0; i < K; i++)
     {
         int n, p; cin >> n >> p;
-        v[i] = {n, p};
+        coin[i] = n;
+        coinNum[i] = p;
     }
-    sort(v.begin(), v.end(), greater<>());
+    memset(d, -1, sizeof(d));
+    cout << dp(0, 0);
 
-
-    for(int i = 0; i < K; i++)
-    {
-        coin[i] = v[i].first;
-        coinNum[i] = v[i].second;
-        d[i][coin[i]] = 1;
-    }
-
-    for(int i = 1; i <= T; i++)
-    {
-        for(int j = 0; j < K; j++)
-        {
-            int c = coin[j];
-            if(i-c <= 0) continue;
-
-            for(int k = 0; k <= j; k++)
-            {
-                d[j][i] += d[k][i-c];
-            }
-
-            if((i-c) / c == coinNum[j]) d[j][i]--;
-        }
-    }
-
-    int answer = 0;
-    for(int i = 0; i < K; i++) answer += d[i][T];
-    cout << answer;
-
-//    for(int r = 0; r < K; r++)
-//    {
-//        for(int c = 1 ; c <= T; c++)
-//        {
-//            cout << d[r][c] << ' ';
-//        } cout << endl;
-//    }
 }
