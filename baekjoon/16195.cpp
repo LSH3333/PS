1377d80770f105a616573bc32987802a8c7b4b78
LSH3333 <dltpgustpgus@gmail.com>
2022-09-02 14:12:09 +0900

    2022.09.02

---
+++ baekjoon/16195.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+long long d[1010][1010];
+#define MOD 1000000009
+
+int main()
+{
+    ios::sync_with_stdio(false); cin.tie(NULL);
+
+    d[1][1] = 1;
+    d[2][1] = 1; d[2][2] = 1;
+    d[3][1] = 1; d[3][2] = 2; d[3][3] = 1;
+
+    for(int i = 4; i <= 1000; i++)
+    {
+        for(int j = 2; j <= 1000; j++)
+        {
+            d[i][j] += (d[i-1][j-1] % MOD) + (d[i-2][j-1] % MOD) + (d[i-3][j-1] % MOD);
+        }
+    }
+
+    int T; cin >> T;
+    while(T--)
+    {
+        int N, M; cin >> N >> M;
+        long long ans = 0;
+        for(int i = 1; i <= M; i++)
+        {
+            ans += d[N][i] % MOD;
+        }
+        cout << ans % MOD << '\n';
+    }
+
+}
