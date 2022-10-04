6799462b169c164c3c1f39a7e64cd13d79f42926
LSH3333 <dltpgustpgus@gmail.com>
2022-10-04 14:36:53 +0900

    2022.10.04

---
+++ baekjoon/2250.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int N;
+
+struct Node
+{
+    Node *left;
+    Node *right;
+};
+
+Node nodePool[10010];
+int mark[10010];
+int colIdx = 1;
+
+void InOrder(Node *node, int level)
+{
+    if(node == nullptr) return;
+
+    InOrder(node->left, level+1);
+    mark[colIdx++] = level;
+    InOrder(node->right, level+1);
+}
+
+int FindRoot(const vector<int> &parentNum)
+{
+    for(int i = 1; i <= N; i++)
+    {
+        if(parentNum[i] == 0) return i;
+    }
+}
+
+int main()
+{
+    ios::sync_with_stdio(false); cin.tie(NULL);
+    cin >> N;
+    vector<int> parentNum(N+1, 0);
+    for(int i = 1; i <= N; i++)
+    {
+        int a, b, c; cin >> a >> b >> c;
+        Node *left, *right;
+        if(b != -1) left = &nodePool[b];
+        else left = nullptr;
+        if(c != -1) right = &nodePool[c];
+        else right = nullptr;
+
+        nodePool[a] = {left, right};
+
+        parentNum[b]++;
+        parentNum[c]++;
+    }
+
+    int root = FindRoot(parentNum);
+    InOrder(&nodePool[root], 1);
+
+    // col min, max
+    pair<int,int> levelMark[10010];
+    int maxLevel = 0;
+    for(int i = 1; i <= N; i++)
+    {
+        levelMark[i] = {20000, 0};
+        maxLevel = max(maxLevel, mark[i]);
+    }
+
+    for(int col = 1; col <= N; col++)
+    {
+        int level = mark[col];
+        int minCol = min(levelMark[level].first, col);
+        int maxCol = max(levelMark[level].second, col);
+        levelMark[level] = {minCol, maxCol};
+    }
+
+    int ansWidth = 0, ansLevel = 0;
+    for(int i = 1; i <= maxLevel; i++)
+    {
+        int width = levelMark[i].second - levelMark[i].first + 1;
+        if(width > ansWidth)
+        {
+            ansWidth = width;
+            ansLevel = i;
+        }
+    }
+    cout << ansLevel << ' ' << ansWidth;
+}
