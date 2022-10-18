80ff3faab6184156ca8df33a990eb06e8390ffbb
LSH3333 <dltpgustpgus@gmail.com>
2022-10-18 15:50:48 +0900

    2022.10.18

--- baekjoon/11049.cpp
+++ baekjoon/11049.cpp
@@ -1,50 +1,32 @@
 #include <iostream>
-#include <vector>
 using namespace std;
+
+#define INF 1000000000
 
 int N;
 pair<int,int> arr[510];
-bool visited[510];
-int answer = 2100000000;
-
-void dfs(vector<int> &v, int depth, int sum)
-{
-    if(depth == N-1)
-    {
-        answer = min(answer, sum);
-        return;
-    }
-
-    for(int i = 0; i < N-1; i++)
-    {
-        if(visited[i]) continue;
-
-        visited[i] = true;
-        v.push_back(i);
-        pair<int,int> tmp1 = arr[i], tmp2 = arr[i+1];
-        int add = (arr[i].first * arr[i].second * arr[i+1].second);
-        arr[i] = {arr[i].first, arr[i+1].second};
-        arr[i+1] = arr[i];
-
-        dfs(v, depth+1, sum + add);
-
-        arr[i] = tmp1;
-        arr[i+1] = tmp2;
-        v.pop_back();
-        visited[i] = false;
-    }
-}
+int d[510][510];
 
 int main()
 {
     ios::sync_with_stdio(false); cin.tie(NULL);
     cin >> N;
-    for(int i = 0; i < N; i++)
+    for(int i = 1; i <= N; i++)
     {
         cin >> arr[i].first >> arr[i].second;
     }
 
-    vector<int> v;
-    dfs(v, 0, 0);
-    cout << answer;
+    for(int i = 1; i < N; i++) // 크기
+    {
+        for(int j = 1; j+i <= N; j++) // 시작 인덱스
+        {
+            d[j][j+i] = INF;
+            for(int k = j; k < j+i; k++)
+            {
+                d[j][j+i] = min(d[j][j+i], d[j][k] + d[k+1][i+j] + (arr[j].first * arr[k].second * arr[j+i].second));
+            }
+        }
+    }
+
+    cout << d[1][N];
 }
