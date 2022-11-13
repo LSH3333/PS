5878499f8d30086f9dc0698b9ec37e594641e51b
LSH3333 <dltpgustpgus@gmail.com>
2022-11-13 12:19:39 +0900

    Java Call by value

---
+++ workspace/JavaCallByValue.java
@@ -0,0 +1,32 @@
+import java.io.IOException;
+
+class Type {
+    public int num;
+    public int getNum() {
+        return num;
+    }
+    public void setNum(int _num) {
+        num = _num;
+    }
+}
+
+public class Main
+{
+    public static void main(String[] args) throws IOException
+    {
+        Type a = new Type();
+        a.setNum(1);
+        foo(a);
+        System.out.println(a.getNum()); // 1
+    }
+
+    static void foo(Type type) {
+        type = new Type();
+        type.setNum(2);
+    }
+}
+
+
+//    static void foo(Type type) {
+//        type.setNum(2);
+//    }
