6bc077f600217a928b1c2e68ae3b234bfbd5d77b
LSH3333 <dltpgustpgus@gmail.com>
2022-12-02 14:36:37 +0900

    2022.11.02

--- baekjoon/1062.cpp
+++ baekjoon/1062.cpp
@@ -4,60 +4,53 @@
 using namespace std;
 
 int N, K;
+bool mark[26];
+vector<string> words;
+int answer;
+
+bool CheckWord(string word) {
+    for(auto x : word) {
+        if(!mark[x-'a']) return false;
+    }
+    return true;
+}
+
+void dfs(int idx, int depth) {
+    if(depth == K-5) {
+        int cnt = 0;
+        for(auto word : words) {
+            if(CheckWord(word)) cnt++;
+        }
+        answer = max(answer, cnt);
+
+        return;
+    }
+
+    for(int i = idx; i < 26; i++) {
+        if(mark[i]) continue;
+        mark[i] = true;
+        dfs(i, depth+1);
+        mark[i] = false;
+    }
+}
 
 int main() {
     ios::sync_with_stdio(false); cin.tie(NULL);
     cin >> N >> K;
-    vector<string> words;
-    vector<pair<int,char>> cnt(26);
-    for(int i = 0; i < 26; i++) cnt[i].second = char('a'+i);
-
     for(int i = 0; i < N; i++) {
         string word; cin >> word;
-        sort(word.begin(), word.end());
-        word.erase(unique(word.begin(), word.end()), word.end());
         words.push_back(word);
     }
 
-    // 각 알파벳이 몇개의 단어에 포함 되는지 카운트
-    for(int i = 0; i < 26; i++) {
-        char c = char('a' + i);
+    // a,n,t,i,c
+    mark['a'-'a'] = true;
+    mark['n'-'a'] = true;
+    mark['t'-'a'] = true;
+    mark['i'-'a'] = true;
+    mark['c'-'a'] = true;
 
-        for(const auto &word : words) {
-            for(auto x : word) {
-                if(x == c) {
-                    cnt[i].first++;
-                    break;
-                }
-            }
-        }
-    }
-    sort(cnt.begin(), cnt.end(), greater<>());
 
-    for(int i = 0; i < K; i++) {
-        char c = cnt[i].second;
+    dfs(0, 0);
 
-        for(auto &word : words) {
-            for(auto it = word.begin(); it != word.end(); it++) {
-                if(*it == c) {
-                    word.erase(it);
-                    break;
-                }
-            }
-        }
-    }
-    for(auto x : cnt) {
-        cout << x.first << ',' << x.second << "  ";
-    } cout << endl;
-
-    for(auto x : words) {
-        cout << x << endl;
-    }
-
-    int answer = 0;
-    for(auto x : words) {
-        if(x.empty()) answer++;
-    }
     cout << answer;
-
 }
