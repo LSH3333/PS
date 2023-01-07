47a24a444cec85abea53e986a67003eb006e73f1
LSH3333 <dltpgustpgus@gmail.com>
2023-01-07 15:31:47 +0900

    2023.01.07

--- baekjoon/2457.cpp
+++ baekjoon/2457.cpp
@@ -4,10 +4,8 @@
 using namespace std;
 
 int N, answer;
-bool found = true;
 int goalBegin, goalEnd;
 int monthday[] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-bool mark[400][400];
 
 int DateToDay(int month, int day) {
     int res = 0;
@@ -16,74 +14,47 @@ int DateToDay(int month, int day) {
     return res;
 }
 
-// second 기준 오름차순 정렬, first 기준 오름차순 정렬
-bool cmp(const pair<int,int> &a, const pair<int,int> &b) {
-    if(a.second == b.second) return a.first < b.first;
-    return a.second < b.second;
-}
-
-
 int main() {
-//    ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
     cin >> N;
-    vector<pair<int,int>> v1(N);
-    vector<pair<int,int>> v2(N);
+    vector<pair<int,int>> v(N);
     for(int i = 0; i < N; i++) {
         int a, b, c, d; cin >> a >> b >> c >> d;
-        v1[i] = {DateToDay(a, b), DateToDay(c, d) };
-        v2[i] = v1[i];
+        v[i] = {DateToDay(a, b), DateToDay(c, d) };
     }
 
-    sort(v1.begin(), v1.end());
-    sort(v2.begin(), v2.end(), cmp);
+    sort(v.begin(), v.end());
     goalBegin = DateToDay(3,1);
     goalEnd = DateToDay(11,30);
 
-    cout << "goal: " << goalBegin << ' ' << goalEnd << endl;
-    for(auto x : v1) {
-        cout << x.first << ' ' << x.second << endl;
-    }cout<<endl;
-
-    for(auto x : v2) {
-        cout << x.first << ' ' << x.second << endl;
-    }cout<<endl;
-
-    int left = 0, right = N-1;
-    while(true) {
-        // LEFT
-        while(left < N-1) {
-            if(left >= N || v1[left].first > goalBegin) {
-                left--;
-                if(mark[v1[left].first][v1[left].second]) { left++; continue; }
-                mark[v1[left].first][v1[left].second] = true;
-                break;
-            }
-            left++;
+    // 3.1 이전 시작 꽃 중 가장 긴 꽃 찾음
+    int right=-1;
+    int idx;
+    for(idx = 0; idx < N; idx++) {
+        if(v[idx].first > goalBegin) break;
+        if(v[idx].second > right) {
+            right = v[idx].second;
         }
-        goalBegin = v1[left].second;
-        answer++;
-        cout << "1: " << goalBegin << ' ' << goalEnd << endl;
-        if(left >= right && goalBegin <= goalEnd) { found = false; break; }
-        if(goalBegin > goalEnd) break;
+    }
+    answer = 1;
 
-        // RIGHT
-        if(left >= right) {found = false; break;}
-        while(right > 0) {
-            if(right < 0 || v2[right].second <= goalEnd) {
-                right++;
-                if(mark[v2[right].first][v2[right].second]) { right--; continue; }
-                mark[v2[right].first][v2[right].second] = true;
-                break;
+    //
+    while(right <= goalEnd) {
+        int maxVal = 0, maxIdx = 0;
+        for(; idx < N; idx++) {
+            if(v[idx].first > right) break;
+            if(v[idx].second > maxVal) {
+                maxVal = v[idx].second;
+                maxIdx = idx;
             }
-            right--;
         }
-        goalEnd = v2[right].first-1;
+        if(maxVal == 0) break;
+
         answer++;
-        cout << "2: " << goalBegin << ' ' << goalEnd << endl;
-        if(left >= right && goalBegin <= goalEnd) { found = false; break; }
-        if(goalBegin > goalEnd) break;
+        right = v[maxIdx].second;
     }
 
-    if(!found) cout << 0;
+    if(right <= goalEnd) cout << 0;
     else cout << answer;
+
 }
