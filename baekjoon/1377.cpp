878a750038d8286f56e379f1a5f83287501cc10d
LSH3333 <dltpgustpgus@gmail.com>
2023-01-13 15:07:11 +0900

    2023.01.13

--- baekjoon/1377.cpp
+++ baekjoon/1377.cpp
@@ -4,30 +4,21 @@
 using namespace std;
 
 int N;
-int mark[1000010];
 
 // 구하는것 -> 몇번만에 정렬되는가
-
-
 int main() {
     ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
     cin >> N;
-    vector<int> before(N);
-    vector<int> after(N);
-    for(int i = 0; i <= 1000000; i++) mark[i] = 2000000;
+    vector<pair<int, int>> v(N);
     for(int i = 0; i < N; i++) {
         int n; cin >> n;
-        before[i] = n;
-        after[i] = n;
-        mark[n] = min(mark[n], i);
+        v[i] = {n, i};
     }
-    sort(after.begin(), after.end());
-
-    int answer = -1;
+    sort(v.begin(), v.end());
+    
+    int answer = 0;
     for(int i = 0; i < N; i++) {
-        int beforeIdx = mark[after[i]];
-        answer = max(answer, i-beforeIdx);
+        answer = max(answer, v[i].second - i);
     }
-
     cout << answer+1;
 }
