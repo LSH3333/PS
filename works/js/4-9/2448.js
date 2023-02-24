e2c077ac6ea033f7c17e5256a124eee5767c6157
LSH <dltpgustpgus@gmail.com>
2023-02-24 21:21:01 +0900

    2023.02.24

--- works/js/4-9/2448.js
+++ works/js/4-9/2448.js
@@ -1,57 +1,43 @@
 let fs = require('fs');
-// let input = fs.readFileSync(0).toString().trim().split('\n')
-let input = fs.readFileSync('input.txt').toString().split('\n')
+let input = fs.readFileSync(0).toString().trim().split('\n')
+// let input = fs.readFileSync('input.txt').toString().split('\n')
 
-let H = parseInt(input[0])
-let W = 2 * H - 1
-let arr = new Array(6200).fill(0).map(() => new Array(6200).fill(' '))
+let N = parseInt(input[0])
+let H = N, W = 2 * H - 1
+let arr = new Array(H).fill(0).map(() => new Array(W).fill(' '))
 
+MakeTriangle(3, 0, Math.floor(W / 2) - 1) 
+Print()
 
-arr[0][2] = '*'
-arr[1][1] = '*'
-arr[1][3] = '*'
-for(i = 0; i < 5; i++) {
-    arr[2][i] = '*'
-}
-
-let l = 3 
-while(l < H) {
-    Copy(l) 
-    l *= 2
-}
-
-let answer = ""
-for(i = 0; i < H; i++) {
-    for(j = 0; j < W; j++) {
-        answer += arr[i][j] 
-    }
-    answer += '\n'
-}
-console.log(answer)
-
-
-
-function Copy(h) {
-    let w = 2 * h - 1
-
-    for(i = 0; i < h; i++) {
-        memcpy(h+i, 0, i, w) 
-        memcpy(h+i, w+1, i, w)
-        memset(i, ' ', w)
-    }
-    for(i = 0; i < h; i++) {
-        memcpy(i, Math.floor(w/2)+1, h+i, w)
+function Print() {
+    for(i = 0; i < H; i++) {
+        let ans = ""
+        for(j = 0; j < W; j++) {
+            ans += arr[i][j] 
+        } 
+        console.log(ans)
     }
 }
 
-function memcpy(dest_r, dest_c, src, w) {
-    for(i = 0; i < w; i++) {
-        arr[dest_r][dest_c+i] = arr[src][i]
+function MakeTriangle(n, _r, _c) {
+    if(n > N) return 
+
+    if(n == 3) {
+        w = Math.floor(W/2)
+        arr[0][w] = '*' 
+        arr[1][w-1] = '*'; arr[1][w+1] = '*';
+        for(i = w-2; i <= w+2; i++) arr[2][i] = '*'
+        MakeTriangle(n*2, 0, w-2) 
+    }
+    else {
+        let div = Math.floor(n/2)
+        for(r = _r; r < _r + div; r++) {
+            for(c = _c; c < _c + (div*2-1); c++) {
+                arr[r+div][c-div] = arr[r][c];
+                arr[r+div][c+div] = arr[r][c];
+            }
+        }
+        MakeTriangle(n*2, 0, _c - div) 
     }
 }
 
-function memset(idx, c, len) {
-    for(i = 0; i < len; i++) {
-        arr[idx] = c 
-    }
-}
