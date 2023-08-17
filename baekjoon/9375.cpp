08424b33003c10c0a5b20f430a204e01c94fe287
LSH <dltpgustpgus@gmail.com>
2023-08-17 20:09:05 +0900

    2023.08.17

--- baekjoon/9375.cpp
+++ baekjoon/9375.cpp
@@ -1,66 +1,26 @@
 #include <iostream>
-#include <vector>
 #include <map>
-#include <set>
 using namespace std;
 
-int answer;
-
-// true ?? ? ?? ??
-bool FindDup(const vector<string> &res) {
-    int before = (int)res.size();
-    set<string> s;
-    for(const auto &x : res) {
-        s.insert(x);
-    }
-
-    if(before != s.size()) {
-        return true;
-    }else {
-        return false;
-    }
-}
-
-void dfs(const vector<pair<string,string>> &v,int depth, int goalDepth, int idx,
-         vector<string> &res) {
-
-    if (depth == goalDepth) {
-        if (!FindDup(res)) {
-            answer++;
-        }
-        return;
-    }
-
-    for(int i = idx; i < v.size(); i++) {
-        res.push_back(v[i].second);
-        dfs(v,  depth+1, goalDepth, i, res);
-        res.pop_back();
-    }
-
-}
 
 int main() {
     ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 
     int T; cin >> T;
     while(T--) {
-        answer = 0;
         int N; cin >> N;
-        vector<pair<string,string>> v;
-        map<string,bool> mark;
+        map<string, int> m;
         for(int i = 0; i < N; i++) {
             string a, b; cin >> a >> b;
-            v.push_back({a, b});
+            m[b]++;
         }
 
-
-
-        for(int goalDepth = 1; goalDepth <= v.size(); goalDepth++) {
-            vector<string> res;
-            dfs(v, 0, goalDepth, 0, res);
+        int answer = 1;
+        for (const auto &item: m) {
+            answer *= item.second + 1;
         }
 
-        cout << answer << '\n';
+        cout << answer-1 << '\n';
     }
 
 
