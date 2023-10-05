c911a6a8b2da80fb7391c5e20e6eb753dcbe4162
LSH <dltpgustpgus@gmail.com>
2023-10-05 20:57:53 +0900

    2023.10.05

--- baekjoon/2290.cpp
+++ baekjoon/2290.cpp
@@ -1,78 +1,201 @@
 #include <iostream>
+#include <vector>
+#include <string>
 using namespace std;
 
 int S;
 string N;
 int R, C;
-char b[25][150];
+char b[25][200];
 
-void One(int _r, int _c) {
-    for(int r = 1; r < 1+S; r++) {
-        b[r][_c+C] = '|';
-    }
-    for(int r = 2+S; r < 2+S+S; r++) {
-        b[r][_c+C] = '|';
+
+void Horz1(vector<vector<char>> &number) {
+    for(int c = 1; c < 1 + S; c++) {
+        number[0][c] = '-';
     }
 }
 
-void Two(int _r, int _c) {
-    for(int c = _c + 1; c < _c + 1 + S; c++) {
-        b[0][c] = '-';
-        b[1+S][c] = '-';
-        b[2+S+S][c] = '-';
+void Horz2(vector<vector<char>> &number) {
+    for(int c = 1; c < 1 + S; c++) {
+        number[1+S][c] = '-';
     }
+}
+
+void Horz3(vector<vector<char>> &number) {
+    for(int c = 1; c < 1 + S; c++) {
+        number[2+S+S][c] = '-';
+    }
+}
+
+void Vert1(vector<vector<char>> &number) {
     for(int r = 1; r < 1+S; r++) {
-        b[r][_c+C] = '|';
+        number[r][0] = '|';
     }
+}
+
+void Vert2(vector<vector<char>> &number) {
+    for(int r = 1; r < 1+S; r++) {
+        number[r][C-1] = '|';
+    }
+}
+
+void Vert3(vector<vector<char>> &number) {
     for(int r = R-2; r > R-2-S; r--) {
-        b[r][_c] = '|';
+        number[r][0] = '|';
     }
 }
 
-void Three(int _r, int _c) {
-    for(int c = _c + 1; c < _c + 1 + S; c++) {
-        b[0][c] = '-';
-        b[1+S][c] = '-';
-        b[2+S+S][c] = '-';
-    }
+void Vert4(vector<vector<char>> &number) {
     for(int r = 1; r < 1+S; r++) {
-        b[r][_c+C] = '|';
-        b[r+S+1][_c+C] = '|';
+        number[r+S+1][C-1] = '|';
     }
 }
 
-
-
-void Draw(int _r, int _c, int num) {
-
+void One(vector<vector<char>> &number) {
+    Vert2(number);
+    Vert4(number);
 }
 
-void Print() {
-    for(int r = 0; r < R; r++) {
-        for(int c = 0; c < C * int(N.size()) + int(N.size()); c++) {
-            cout << b[r][c];
-        } cout << '\n';
+void Two(vector<vector<char>> &number) {
+    Horz1(number);
+    Horz2(number);
+    Horz3(number);
+    Vert2(number);
+    Vert3(number);
+}
+
+void Three(vector<vector<char>> &number) {
+    Horz1(number);
+    Horz2(number);
+    Horz3(number);
+    Vert2(number);
+    Vert4(number);
+}
+
+void Four(vector<vector<char>> &number) {
+    Vert1(number);
+    Vert2(number);
+    Horz2(number);
+    Vert4(number);
+}
+
+void Five(vector<vector<char>> &number) {
+    Horz1(number);
+    Horz2(number);
+    Horz3(number);
+    Vert1(number);
+    Vert4(number);
+}
+
+void Six(vector<vector<char>> &number) {
+    Horz1(number);
+    Horz2(number);
+    Horz3(number);
+    Vert1(number);
+    Vert3(number);
+    Vert4(number);
+}
+
+void Seven(vector<vector<char>> &number) {
+    Horz1(number);
+    Vert2(number);
+    Vert4(number);
+}
+
+void Eight(vector<vector<char>> &number) {
+    Horz1(number);
+    Horz2(number);
+    Horz3(number);
+    Vert1(number);
+    Vert2(number);
+    Vert3(number);
+    Vert4(number);
+}
+
+void Nine(vector<vector<char>> &number) {
+    Horz1(number);
+    Horz2(number);
+    Horz3(number);
+    Vert1(number);
+    Vert2(number);
+    Vert4(number);
+}
+
+void Zero(vector<vector<char>> &number) {
+    Horz1(number);
+    Horz3(number);
+    Vert1(number);
+    Vert2(number);
+    Vert3(number);
+    Vert4(number);
+}
+
+vector<vector<char>> DrawNumber(char num) {
+    vector<vector<char>> number(R, vector<char>(C, ' '));
+
+    if(num == '0') {
+        Zero(number);
+    } else if (num == '1') {
+        One(number);
+    } else if (num == '2') {
+        Two(number);
+    }else if (num == '3') {
+        Three(number);
+    }else if (num == '4') {
+        Four(number);
+    }else if (num == '5') {
+        Five(number);
+    }else if (num == '6') {
+        Six(number);
+    }else if (num == '7') {
+        Seven(number);
+    }else if (num == '8') {
+        Eight(number);
+    }else if (num == '9') {
+        Nine(number);
     }
+
+    return number;
 }
 
 void Init() {
-    for(int r = 0; r < R; r++) {
-        for(int c = 0; c < C * int(N.size()) + int(N.size()); c++) {
+    for(int r = 0; r < 25; r++) {
+        for(int c = 0; c < 200; c++) {
             b[r][c] = ' ';
         }
     }
 }
 
-// ??? ????, ? ??? ????? ?? (? 7?), vector<vector<char>> ? ??? ??? ??
+
 int main() {
+    Init();
     cin >> S >> N;
 
     R = 2 * S + 3;
     C = S + 2;
 
-    Init();
+//    cout << R << ' ' << C << endl;
 
-    Three(0,0);
+    vector<vector<vector<char>>> v;
 
-    Print();
+    for(char c : N) {
+        auto res = DrawNumber(c);
+        v.push_back(res);
+    }
+
+    for(int i = 0; i < (int)v.size(); i++) {
+
+        for(int r = 0; r < R; r++) {
+            for(int c = i*C+i; c < i*C+i+C; c++) {
+                b[r][c] = v[i][r][c-(i*C+i)];
+            }
+        }
+    }
+
+    // print
+    for(int r = 0; r < R; r++) {
+        for(int c = 0; c < C * v.size() + v.size()-1; c++) {
+            cout << b[r][c];
+        } cout << '\n';
+    }
 }
